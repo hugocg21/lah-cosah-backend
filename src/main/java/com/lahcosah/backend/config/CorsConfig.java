9ba3ebe4dd3701b4035e09f8d31fa43b01f3c2b2
hugocg21 <hugocastanogonzalez@gmail.com>
2024-08-19 17:53:45 +0200

    CorsConfig file updated

--- src/main/java/com/lahcosah/backend/config/CorsConfig.java
+++ src/main/java/com/lahcosah/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://lahcosahnuehtrah.netlify.app")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
