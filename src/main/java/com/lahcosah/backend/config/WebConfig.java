a7b3a7af90d9f613502d61ef39f07e04f3930241
hugocg21 <hugocastanogonzalez@gmail.com>
2024-08-19 17:48:21 +0200

    WebConfig file updated to fix some CORS problems

--- src/main/java/com/lahcosah/backend/config/WebConfig.java
+++ src/main/java/com/lahcosah/backend/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.lahcosah.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,16 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://lahcosahnuehtrah.netlify.app")
+                .allowedOrigins("https://lahcosahnuehtrah.netlify.app") // Reemplaza con tu dominio de Netlify
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Configuración para servir recursos estáticos desde una ubicación en el sistema de archivos local.
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/ruta/a/uploads/"); // Asegúrate de que la ruta sea válida en Heroku.
     }
 }
